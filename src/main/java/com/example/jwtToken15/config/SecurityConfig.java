101c182e5128a0071135ea5bd111e307e82ec38c
Shubham Patil <patilshubham6803@gmail.com>
2026-07-22 12:30:00 +0530

    security exception added

--- src/main/java/com/example/jwtToken15/config/SecurityConfig.java
+++ src/main/java/com/example/jwtToken15/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.example.jwtToken15.config;
 
+import com.example.jwtToken15.security.CustomAccessDeniedHandler;
+import com.example.jwtToken15.security.CustomAuthenticationEntryPoint;
+import com.example.jwtToken15.security.JwtAuthenticationFilter;
 import com.example.jwtToken15.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,25 +16,36 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
-
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService){
-        this.customUserDetailsService = customUserDetailsService;
-    }
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
         http.csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers("/api/private/**").hasRole("ADMIN")
                         .requestMatchers("/api/user/**").hasAnyRole("USER","ADMIN")
                         .anyRequest().authenticated())
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler))
+
                 .sessionManagement(session -> session.
-                        sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
