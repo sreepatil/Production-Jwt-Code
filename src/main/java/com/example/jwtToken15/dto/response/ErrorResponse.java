617544fdf063c1a411858b8a167217ff4e0fb436
Shubham Patil <patilshubham6803@gmail.com>
2026-07-22 12:30:45 +0530

    veriable name changes to path

--- src/main/java/com/example/jwtToken15/dto/response/ErrorResponse.java
+++ src/main/java/com/example/jwtToken15/dto/response/ErrorResponse.java
@@ -2,5 +2,5 @@ package com.example.jwtToken15.dto.response;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(String description, int status, String error, String message, LocalDateTime timestamp) {
+public record ErrorResponse(String path, int status, String error, String message, LocalDateTime timestamp) {
 }
